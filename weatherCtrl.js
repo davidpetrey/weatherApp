1adefee6c4f706686c4245940a4ca3b3ec08c63d
Andrew Kemp <andrewtkemp@gmail.com>
2016-09-21 07:11:35 -0500

    updates

--- weatherCtrl.js
+++ weatherCtrl.js
@@ -4,10 +4,14 @@ angular.module('weatherApp').controller('weatherCtrl', function($scope, weatherS
             $scope.getWeather = function(city){
               console.log(city)
               weatherService.getWeather(city).then(function(response){
+                console.log(response);
                 $scope.cityWeather = response;
                 $scope.weatherWeek = response.list;
-                console.log($scope.weatherWeek);
               });
+              weatherService.getWeatherIcons(city).then(function(response){
+                $scope.weatherIcons = response.daily.data;
+                  console.log("Icons", $scope.weatherIcons);
+              })
             }
 
             $scope.roundTemp = function(temp) {
--- weatherService.js
+++ weatherService.js
@@ -2,6 +2,7 @@ angular.module("weatherApp").service("weatherService", function($http, $q) {
 
 
     var openWeatherKey = "c7969564d2884537f64d9c5750d8b275";
+    // var darkSkyKey = "ae09ed9744cc4113f2cb0f42d6f17fb8";
 
     this.getWeather = function(city){
         	var deferred = $q.defer();
@@ -16,10 +17,29 @@ angular.module("weatherApp").service("weatherService", function($http, $q) {
             }
         	}).then(function(result){
         		cityWeather = result.data;
-            console.log(cityWeather);
             deferred.resolve(cityWeather);
           });
         	return deferred.promise;
         };
+
+        // this.getWeatherIcons = function(city){
+        //     	var deferred = $q.defer();
+        //     	var cityIcons;
+        //     	var cityIconsFinished;
+        //     	$http({
+        //     		method: "GET",
+        //     		url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherKey
+        //     	}).then(function(result){
+        //     		cityIcons = result.data;
+        //     		$http({
+        //     			method: "JSONP",
+        //     			url: "https://api.forecast.io/forecast/" + darkSkyKey + "/" + cityIcons.coord.lat + "," + cityIcons.coord.lon + "?callback=JSON_CALLBACK"
+        //     		}).then(function(results){
+        //     			cityIconsFinished = results.data;
+        //     			deferred.resolve(cityIconsFinished);
+        //     		});
+        //     	});
+        //     	return deferred.promise;
+        //     };
       });
 // send back cityWeather results AND your forecast icon results
